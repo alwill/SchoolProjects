09e6751a355986b2145d5142a8c212901a994935
Alex <alwill@siue.edu>
2015-09-24 05:06:03 -0500

    Adding files for Project 2 in cs340

---
+++ cs340/Project2/BinarySearchTree.java
@@ -0,0 +1,5 @@
+/**
+ * Created by AlexWill on 9/24/15.
+ */
+public class BinarySearchTree {
+}
---
+++ cs340/Project2/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by AlexWill on 9/24/15.
+ */
+public class Main {
+
+    public static void main(String[] args)
+    {
+        
+    }
+}
