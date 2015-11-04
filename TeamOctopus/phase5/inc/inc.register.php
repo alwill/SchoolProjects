e0e8af6d2f32afd17be3a2080845e056e86dc202
Jake King <jakking@siue.edu>
2015-11-04 12:10:01 -0600

    updated user sql and added and error message if passwords do not match

--- TeamOctopus/phase5/inc/inc.register.php
+++ TeamOctopus/phase5/inc/inc.register.php
@@ -4,22 +4,29 @@ if(isset($_POST['register']))
     register();
 
 function register(){
-    global $con;
-    try {
-        $sql = $con->prepare("INSERT INTO `test`.`users` (`id`, `userid`, `username`, `password`, `email`, `favorites`) VALUES (NULL, 0, :username, :password, :email, NULL)");
-        $sql->bindParam(':username', $_POST['regUsername']);
-        $sql->bindParam(':password', $_POST['regPassword']);
-		$sql->bindParam(':email', $_POST['regEmail']);
-        $sql->execute();
-        if($sql->rowCount() == 1){
-            session_start();
-            $_SESSION['username'] = $_POST['username'];
-            $_SESSION['loggedin'] = true;
-            header("Location: /");
-            exit();
-        }
-    } catch(PDOException $e){
-        echo $e->getMessage();
-    }
+	if($_POST["regPassword"] == $_POST["regRepassword"]){
+		global $con;
+		try {
+			$sql = $con->prepare("INSERT INTO `test`.`users` (`id`, `userid`, `username`, `password`, `email`, `favorites`) VALUES (NULL, 0, :username, :password, :email, NULL)");
+			$sql->bindParam(':username', $_POST['regUsername']);
+			$sql->bindParam(':password', $_POST['regPassword']);
+			$sql->bindParam(':email', $_POST['regEmail']);
+			$sql->execute();
+			if($sql->rowCount() == 1){
+				session_start();
+				$_SESSION['username'] = $_POST['username'];
+				$_SESSION['loggedin'] = true;
+				header("Location: /");
+				exit();
+			}
+		} catch(PDOException $e){
+			echo $e->getMessage();
+		}
+	}
+	else{
+		echo '<script type="text/javascript">';
+        echo 'alert ("passwords do not match");';
+		echo '</script>';
+	}
 }
 ?>
