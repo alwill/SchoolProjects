12276787e20d4862c78df68488d77d50c4b58b8e
Jake King <jakking@siue.edu>
2015-11-08 17:14:13 -0600

    made it go to login if you click on my profile and youre not logged in

--- TeamOctopus/phase5/pages/profile.php
+++ TeamOctopus/phase5/pages/profile.php
@@ -3,19 +3,21 @@ session_start();
 $pageTitle="Profile";
 # Checks if logged in. If NOT logged in, the login button stays the same.
 # If IS logged in, the login button changes to logout.
-if(isset($_SESSION['loggedin']))
+if(isset($_SESSION['loggedin'])){
     $loggedin = $_SESSION['loggedin'];
-include_once("$_SERVER[DOCUMENT_ROOT]/phase5/inc/header.php");
-// Body content
-$favoritesArray = explode(',', $_SESSION['favorites']);
-echo '<ul>';
-
-
-foreach ($favoritesArray as &$fav) {
+	$favoritesArray = explode(',', $_SESSION['favorites']);
+	echo '<ul>';
+	foreach ($favoritesArray as &$fav) {
     echo '<li>' . $fav . '</li>';
 }
 echo '</ul>';
+}
+else{
+header("Location: /phase5/pages/login.php");
 
+}
+include_once("$_SERVER[DOCUMENT_ROOT]/phase5/inc/header.php");
+// Body content
 ?>
 <div id="wrap">
 
